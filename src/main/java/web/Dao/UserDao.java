25aa1943ddb5cc16b79996471e3f82bebf4ba0e2
BeeG00se <antipina.sasha2000@icloud.com>
2024-02-16 18:05:51 +0300

    Task is completed

--- src/main/java/web/Dao/UserDao.java
+++ src/main/java/web/Dao/UserDao.java
@@ -1,14 +1,6 @@
 package web.Dao;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import web.model.User;
 
-import javax.validation.Valid;
-import java.util.List;
-
-public interface UserDao {
-    List<User> getAllUsers ();
-    User getUserById(int id);
-    void addUser(User user);
-    void removeUser(int id);
-    void updateUser(@Valid User user);
+public interface UserDao extends JpaRepository<User, Integer> {
 }
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,14 +1,12 @@
 package web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import web.Dao.UserDao;
 import web.model.User;
 
 import javax.validation.Valid;
 import java.util.List;
 @Service
-@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserDao userDao;
@@ -20,26 +18,26 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAllUsers() {
-        return userDao.getAllUsers();
+        return userDao.findAll();
     }
 
     @Override
     public User getUserById(int id) {
-        return userDao.getUserById(id);
+        return userDao.findById(id).get();
     }
 
     @Override
     public void addUser(@Valid User user) {
-        userDao.addUser(user);
+        userDao.save(user);
     }
 
     @Override
     public void removeUser(int id) {
-        userDao.removeUser(id);
+        userDao.deleteById(id);
     }
 
     @Override
     public void updateUser(@Valid User user) {
-        userDao.updateUser(user);
+        userDao.save(user);
     }
 }
